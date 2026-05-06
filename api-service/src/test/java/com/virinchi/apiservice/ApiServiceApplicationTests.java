482f1dc8c981fc2f85e40fa9e19077ad05a24ce5
Virinchi Doddapaneni <virinchidoddapaneni@gmail.com>
2026-05-06 10:35:42 +0800

    Initialize Java Spring Boot Kafka project

---
+++ api-service/src/test/java/com/virinchi/apiservice/ApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virinchi.apiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
